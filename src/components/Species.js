a75b603541b1d939015254de9de4321c5217cda6
Evelyn Sharp <yihongshi90@gmail.com>
2017-08-01 16:28:28 -0600

    compact

--- src/components/Species.js
+++ src/components/Species.js
@@ -11,22 +11,37 @@ class Species extends React.Component {
   // }
 
   getSpecies = () => {
-
     axios.get('http://swapi.co/api/species/1/')
       .then( res => this.setState( { species: res.data } ) )
       .catch( err => console.log(err) )
   }
 
+  useCompact = () => {
+    let { species } = this.state;
+    let newArr = species.skin_colors.split(', ').map( s => {
+      if ( s === 'asian')
+        return true
+      return false
+    })
+    return _.compact(newArr).length;
+  }
+
   render() {
     let { species } = this.state;
+    console.log(species);
     return(
       <div>
         Species
         <button onClick={this.getSpecies}>Get Species</button>
         <div>
           { !_.isEmpty(species) &&
-            <div>Chunck: <span>{_.chunk(species.films, 2)[0].join('***')}</span></div>
-            
+            <div>
+              <div>Chunck: <span>{_.chunk(species.films, 2)[0].join('***')}</span></div>
+              <div>Compact (Asian Count): <span>
+                  { this.useCompact() }
+                </span>
+              </div>
+            </div>
           }
         </div>
       </div>
